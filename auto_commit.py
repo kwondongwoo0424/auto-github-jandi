31aeb2050c29042b33a4f1be0932e7be00568ee1
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-08-22 16:08:27 +0900

    feat: auto commit every 24 hours

--- auto_commit.py
+++ auto_commit.py
@@ -1,14 +1,26 @@
-import fileinput
-import sys
 import os
+import threading
 
 n = 1
- 
-for line in fileinput.input('./README.md', inplace = True):
-    if '커밋' in line:
-        null_text = " " * n
-        line = line.replace(line, f'자동으로 하루에 한번 커밋{null_text}')
-        
-    sys.stdout.write(line)
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def autoCommit():
+    global n
+    f = open("./README.md", 'a', encoding="utf8")
+    f.write(" ")
     
-os.system("clear")
+    os.system("git add README.md")
+    os.system(f'git commit -m "Update README.md {n}"')
+    os.system("git push origin main")
+    
+    n+=1
+
+autoCommit()
+set_interval(autoCommit, 86400) #24시간 마다 실행
